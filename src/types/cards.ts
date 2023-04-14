4095811b1b2306d897c79ec7495ee3a9ec45b9e6
Andrew Nastenko <andrew1991n@gmail.com>
2023-04-15 02:33:43 +0300

    updated tests & fixes

--- src/types/cards.ts
+++ src/types/cards.ts
@@ -1,14 +1,18 @@
-export interface CreateCard {
+import {type AxiosResponse} from 'axios';
+
+export interface CreateCardPayload {
   cardName: string;
   idList: string;
 }
 
-export interface CreateCards {
+export interface CreateCardsPayload {
   count: number;
   idList: string;
 }
 
-export interface UpdateCard {
+export interface UpdateCardPayload {
   cardId: string;
   cardName: string;
 }
+
+export type CardResponse = Promise<AxiosResponse<Record<any, any>>>;
