518c53f1cf9803ce57c36d4b1de54029dc928315
Andrii Nastenko <andrew1991n@gmail.com>
2023-04-20 00:11:57 +0300

    added types and response validation

--- tests/cards/cards.spec.ts
+++ tests/cards/cards.spec.ts
@@ -1,6 +1,6 @@
 import {CardsApiCaller} from 'src/api/cards/cards-api-caller';
 import {DataGenerator} from 'src/helpers/data-generator';
-import {Errors} from 'tests/cards/responses';
+import {Errors, checkCardResponse} from 'tests/cards/responses';
 
 describe('Cards:', () => {
   const cardsApiCaller = new CardsApiCaller();
@@ -17,6 +17,7 @@ describe('Cards:', () => {
   });
 
   it('should create new card', async () => {
+    expect(cardData).toEqual(checkCardResponse({name: cardName}));
     expect(cardData.name).toEqual(cardName);
     expect(Object.keys(cardData)).toHaveLength(34);
   });
@@ -24,6 +25,7 @@ describe('Cards:', () => {
   it('should get card info', async () => {
     const {data, status} = await cardsApiCaller.getCard(cardData.id);
 
+    expect(data).toEqual(checkCardResponse({name: cardName}));
     expect(data.name).toEqual(cardName);
     expect(Object.keys(data)).toHaveLength(31);
     expect(status).toEqual(200);
@@ -36,6 +38,7 @@ describe('Cards:', () => {
       cardId: cardData.id,
     });
 
+    expect(data).toEqual(checkCardResponse({name: newCardName}));
     expect(data.name).toEqual(newCardName);
     expect(Object.keys(data)).toHaveLength(31);
     expect(status).toEqual(200);
