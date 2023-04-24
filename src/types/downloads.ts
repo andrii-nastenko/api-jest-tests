06db5fe7f5937e5c919e085c9319e2967150a7c5
Andrii Nastenko <andrew1991n@gmail.com>
2023-04-24 20:53:38 +0300

    added test for downloading and parsing csv file

---
+++ src/types/downloads.ts
@@ -0,0 +1,11 @@
+export type CSV_REPORT = Array<{
+  Index: string;
+  'Organization Id': string;
+  Name: string;
+  Website: string;
+  Country: string;
+  Description: string;
+  Founded: string;
+  Industry: string;
+  'Number of employees': string;
+}>;
