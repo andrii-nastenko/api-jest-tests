efccbe03cabbb3ca412f720d02fc1a7e698a4031
Andrii Nastenko <andrew1991n@gmail.com>
2023-04-29 16:03:52 +0300

    stability fixes

--- src/helpers/data-generator.ts
+++ src/helpers/data-generator.ts
@@ -3,6 +3,12 @@ import {faker} from '@faker-js/faker';
 export function generateString(length: number): string {
   return faker.random.alphaNumeric(length);
 }
-export function generateStringOfNumbers(digitsCount: number): string {
+export function generateName(): string {
+  return faker.name.firstName();
+}
+export function generateNumber(digitsCount: number): number {
+  return parseInt(faker.random.numeric(digitsCount));
+}
+export function generateStringNumber(digitsCount: number): string {
   return faker.random.numeric(digitsCount);
 }
--- tests/sql-queries.spec.ts
+++ tests/sql-queries.spec.ts
@@ -1,26 +1,34 @@
 import {DbCaller} from 'src/db/db-caller';
+import {generateName, generateNumber} from 'src/helpers/data-generator';
 
 describe('Get sql queries from Users table:', () => {
   const dbCaller = new DbCaller();
+  const id = generateNumber(5);
+  const age = generateNumber(2);
+  const name = generateName();
 
   beforeAll(async () => {
+    await dbCaller
+      .execute({
+        sql: 'CREATE TABLE users (Id INT, Age INT, Name NVARCHAR(20))',
+      })
+      .catch((error) => error);
     await dbCaller.execute({
-      sql: 'CREATE TABLE users (Id INT, Age INT, Name NVARCHAR(20))',
+      sql: `INSERT INTO users VALUES(${id}, ${age}, "${name}")`,
     });
-    await dbCaller.execute({sql: 'INSERT INTO users VALUES(1, 32, "Andrew")'});
   });
 
   afterAll(async () => {
-    await dbCaller.execute({sql: 'DROP TABLE users'});
+    await dbCaller.execute({sql: 'DROP TABLE users'}).catch((error) => error);
     await dbCaller.disconnect();
   });
 
   it('should get user name and age by id', async () => {
     const response = await dbCaller.execute({
-      sql: 'SELECT Name, Age FROM users WHERE Id = 1',
+      sql: `SELECT Name, Age FROM users WHERE Id = ${id}`,
     });
 
     expect(response[0]).toHaveLength(1);
-    expect(response[0]).toIncludeAllMembers([{Name: 'Andrew', Age: 32}]);
+    expect(response[0]).toIncludeAllMembers([{Name: name, Age: age}]);
   });
 });
